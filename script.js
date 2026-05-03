a44d7f4fc1d8aa70770f0708ce34a7d405f241ed
SagarSingh01 <sagarsingh07789@gmail.com>
2026-05-03 13:55:14 +0530

    Enter Key Feature Added

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ function list(e) {
     e.target.parentElement.classList.toggle("list-check");
 }
 
-function Todolist() {
-    let inputValue = document.querySelector(".input-container>input").value;
-    let element = document.createElement("li");
+function Todolist(e) {
+    if (e.key === "Enter") {
+        let inputValue = document.querySelector(".input-container>input").value;
+        let element = document.createElement("li");
 
-    if (inputValue === "" || inputValue === " ") {
-        return alert("Enter Input!");
-    }
+        if (inputValue.trim() === "") {
+            return alert("Enter Input!");
+        }
 
-    element.innerHTML = `<span onclick="list(event)">${inputValue}</span>
+        element.innerHTML = `<span onclick="list(event)">${inputValue}</span>
             <div class="input-edit">
                 <input type="text">
                 <i class="fa-regular fa-floppy-disk" onclick="save(event)"></i>
@@ -21,8 +22,9 @@ function Todolist() {
         <i class="fa-regular fa-trash-can" onclick = "removeList(event)"></i>
         <i class="fa-regular fa-pen-to-square" onclick="Edit(event)"></i> 
             </div>`;
-    document.querySelector(".list-container").append(element);
-    document.querySelector(".input-container>input").value = "";
+        document.querySelector(".list-container").append(element);
+        document.querySelector(".input-container>input").value = "";
+    }
 }
 
 function removeList(e) {
@@ -35,7 +37,7 @@ function Edit(e) {
 
 function save(e) {
     let saveVal = e.target.previousElementSibling.value;
-    if (saveVal === "" || saveVal === " ") return alert("Enter Value to Edit");
+    if (saveVal.trim() === "") return alert("Enter Value to Edit");
     e.target.parentElement.previousElementSibling.innerHTML = saveVal;
     e.target.parentElement.remove();
 }
